7e305e9eb40b7983ae8aaa2fa8fcb57b664906e7
ghazwulshaf <ghazwul.s@gmail.com>
2024-12-23 14:51:41 +0800

    change: update homepage layout

--- Controllers/Home/HomeController.cs
+++ Controllers/Home/HomeController.cs
@@ -1,20 +1,33 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using GhazwulShaf.Models;
+using GhazwulShaf.Services;
 
 namespace GhazwulShaf.Controllers.Home;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly AboutProfileService _profileService;
+    private readonly ProjectsService _projectsService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, AboutProfileService profileService, ProjectsService projectsService)
     {
         _logger = logger;
+        _profileService = profileService;
+        _projectsService = projectsService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        var about = await _profileService.GetAsync();
+        var projects = await _projectsService.GetAllAsync();
+
+        ViewBag.AboutDescription = about.Data["Description"];
+        ViewBag.Projects = projects.OrderByDescending(p => p.Id)
+            .Take(3)
+            .ToList();
+
         return View();
     }
 
